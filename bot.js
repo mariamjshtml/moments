3b230762c6605d4567bd7a3235528e55b9da2010
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2020-02-12 23:12:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,14 @@ bot.on("message", msg => {
         const Auser = msg.guild.members.get(msg.content.split(" ")[1]) || msg.mentions.members.first();
         msg.delete(0)
         const Embed = new discord.RichEmbed()
+        .setAuthor("Done", "https://images-ext-2.discordapp.net/external/qZqmfbFGs1UWqmOXggpOG8aM7TguT6rERqk3119FRIc/%3Fv%3D1/https/cdn.discordapp.com/emojis/547902692652220428.gif")
         .setImage(msg.author.displayAvatarURL)
         .setColor('RANDOM');
 
     if(!Auser) return msg.channel.send(Embed).then(msg => msg.delete(10000));
 
     const Aembed = new discord.RichEmbed()
+    .setAuthor("Done", "https://images-ext-2.discordapp.net/external/qZqmfbFGs1UWqmOXggpOG8aM7TguT6rERqk3119FRIc/%3Fv%3D1/https/cdn.discordapp.com/emojis/547902692652220428.gif")
     .setImage(Auser.user.displayAvatarURL)
     .setColor('RANDOM');
     if(Auser) return msg.channel.send(Aembed).then(msg => msg.delete(10000));
