0b0654c00fd855e77d3c4bfc492dff710b626850
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2020-02-14 00:07:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,4 +19,30 @@ bot.on("message", msg => {
     }    
 })
 
+bot.on("message", msg => {
+    const prefix = "-";
+    const cmd = msg.content.split(" ")[0];
+    
+        if(cmd === `${prefix}avatar`) {
+        const Auser = msg.guild.members.get(msg.content.split(" ")[1]) || msg.mentions.members.first();
+        msg.delete(0)
+        const Embed = new discord.RichEmbed()
+        .setAuthor(msg.author.username, "https://images-ext-2.discordapp.net/external/qZqmfbFGs1UWqmOXggpOG8aM7TguT6rERqk3119FRIc/%3Fv%3D1/https/cdn.discordapp.com/emojis/547902692652220428.gif")
+        .setImage(msg.author.displayAvatarURL)
+        .setColor('RANDOM')
+        .setFooter("Requested By " + msg.author.tag, msg.author.displayAvatarURL);
+            
+        if(!Auser) return msg.channel.send(Embed).then(msg => msg.delete(10000));
+        
+            const Aembed = new discord.RichEmbed()
+    .setAuthor(Auser.user.username, "https://images-ext-2.discordapp.net/external/qZqmfbFGs1UWqmOXggpOG8aM7TguT6rERqk3119FRIc/%3Fv%3D1/https/cdn.discordapp.com/emojis/547902692652220428.gif")
+    .setImage(Auser.user.displayAvatarURL)
+    .setColor('RANDOM')
+    .setFooter("Requested By " + msg.author.tag, msg.author.displayAvatarURL);
+    if(Auser) return msg.channel.send(Aembed).then(msg => msg.delete(10000));
+        
+
+    }
+});
+
 bot.login(process.env.BOT_TOKEN);
