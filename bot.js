c9f383e0c24bf710d1863afea51d20bc985dba0f
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2020-02-15 14:23:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,31 @@ const fs = require('fs');
 const gif = require("gif-search");
 const bot = new discord.Client();
 
+//Clear
+bot.on('message', msg => {
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(prefix)) return;
+    let command = msg.content.split(" ")[0];
+    command = command.slice(prefix.length);
+    let args = msg.content.split(" ").slice(1);
+  
+      if(command === "clear") {
+          const emoji = bot.emojis.find("name", "wastebasket")
+      let textxt = args.slice(0).join("");
+      if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+      if (textxt == "") {
+          msg.delete().then
+      msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(3000));
+  } else {
+      msg.delete().then
+      msg.delete().then
+      msg.channel.bulkDelete(textxt);
+          msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+          }    
+      }
+  }
+  });
+
 
 
 
